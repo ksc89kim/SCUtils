fe821f24f8c7533153f5083f11f4dd0dacf08664
ksc89kim <ksc89kim@naver.com>
2022-05-30 20:41:25 +0900

    UIImageView Utils 추가

---
+++ Sources/SCUtils/UIImageView+Base.swift
@@ -0,0 +1,34 @@
+//
+//  UIImageView+Base.swift
+//  
+//
+//  Created by kim sunchul on 2022/05/30.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+
+public extension UIImageView {
+
+  func cropImage() {
+    let frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height)
+
+    let imageView: UIImageView = UIImageView(frame: frame)
+    imageView.backgroundColor = UIColor.clear
+    imageView.contentMode = .scaleAspectFill
+    imageView.image = self.image
+    imageView.layer.masksToBounds = true
+    imageView.layer.cornerRadius = self.layer.cornerRadius
+
+    UIGraphicsBeginImageContext(frame.size)
+    imageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+    let roundedImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+
+    self.image = roundedImage
+  }
+
+}
+
+#endif
