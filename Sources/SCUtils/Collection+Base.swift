79af40e12da2e4873b34b8f32d8f5ff3a16042f6
ksc89kim <ksc89kim@naver.com>
2022-05-30 17:11:37 +0900

    Collection Utils 추가

---
+++ Sources/SCUtils/Collection+Base.swift
@@ -0,0 +1,28 @@
+//
+//  Collection+Base.swift
+//  
+//
+//  Created by kim sunchul on 2022/05/30.
+//
+
+import Foundation
+
+public extension Collection {
+
+  subscript(safe index: Index) -> Element? {
+    return self.indices.contains(index) ? self[index] : nil
+  }
+
+  func group(by size: Int) -> [[Element]]? {
+    guard size > 0, !self.isEmpty else { return nil }
+    var start: Self.Index = self.startIndex
+    var slices: [[Element]] = [[]]
+    while start != self.endIndex {
+      let end = self.index(start, offsetBy: size, limitedBy: self.endIndex) ?? self.endIndex
+      slices.append(Array(self[start..<end]))
+      start = end
+    }
+    return slices
+  }
+  
+}
